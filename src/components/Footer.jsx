f5d327d32e4e12cc9f7e7f5a17c3431a0910baa8
Arman Bhatia <bhat0100@algonquinlive.com>
2025-07-28 00:35:52 -0400

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 // Footer.jsx
 // This component renders the footer section with branding, social media links, and copyright.
 import React from "react";
+import { SiGithub } from "react-icons/si";
 import { Link } from "react-router-dom";
+
 
 function Footer() {
   return (
@@ -19,29 +21,9 @@ function Footer() {
 
           {/* Social media icons section */}
           <div className="flex justify-center md:justify-end space-x-6 md:w-1/3">
-            {/* Facebook icon */}
-            <a
-              href="https://facebook.com"
-              aria-label="Facebook"
-              className="hover:text-white transition"
-            >
-              <svg className="fill-current w-6 h-6" viewBox="0 0 24 24">
-                <path d="M22 12a10 10 0 1 0-11.5 9.9v-7H8v-3h2.5V9.5c0-2.5 1.5-3.9 3.7-3.9 1.1 0 2.3.2 2.3.2v2.5h-1.3c-1.3 0-1.7.8-1.7 1.6V12h3l-.5 3h-2.5v7A10 10 0 0 0 22 12z" />
-              </svg>
-            </a>
-            {/* Twitter icon */}
-            <a
-              href="https://twitter.com"
-              aria-label="Twitter"
-              className="hover:text-white transition"
-            >
-              <svg className="fill-current w-6 h-6" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 0 1-3.1.85A5.3 5.3 0 0 0 22.4.4a10.6 10.6 0 0 1-3.3 1.26 5.3 5.3 0 0 0-9 4.82A15 15 0 0 1 1.6 2.15a5.3 5.3 0 0 0 1.64 7.06 5.27 5.27 0 0 1-2.4-.66v.06a5.3 5.3 0 0 0 4.3 5.2 5.3 5.3 0 0 1-2.4.1 5.3 5.3 0 0 0 4.94 3.68A10.6 10.6 0 0 1 1 19.5a15 15 0 0 0 8.13 2.37c9.76 0 15.1-8.1 15.1-15.1 0-.23 0-.46-.02-.68A10.8 10.8 0 0 0 23 3z" />
-              </svg>
-            </a>
             {/* LinkedIn icon */}
             <a
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/arman-bhatia/"
               aria-label="LinkedIn"
               className="hover:text-white transition"
             >
@@ -54,7 +36,7 @@ function Footer() {
 
         {/* Copyright section at the bottom */}
         <div className="mt-8 pt-4 border-gray-700 border-t text-gray-500 text-sm text-center">
-          &copy; {new Date().getFullYear()} YourCompany. All rights reserved.
+          &copy; {new Date().getFullYear()} ArmanBhatia. All rights reserved.
         </div>
       </div>
     </footer>
